a5dae4a3dc3aa5bbc3ee7e8386b5c383f51f251a
Joel Zapana <joel.zapana@sjsu.edu>
2024-07-05 19:16:45 -0700

    Added more to the project preview, and added more to experience page

--- src/ProjectPreview.js
+++ src/ProjectPreview.js
@@ -1,26 +1,58 @@
 import React, { useState } from 'react';
-import questionPic from './assets/IMG_7794.jpg';
+import markPic from './assets/IMG_7800.jpg';
+import lukePic from './assets/IMG_7802.jpg';
+import johnPic from './assets/IMG_7801.jpg';
 
 const ProjectPreview = () => {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [selectedAnswer, setSelectedAnswer] = useState('');
     const [feedback, setFeedback] = useState('');
     const [showExplanation, setShowExplanation] = useState(false);
   
-    const question = {
-      text: 'According to Mark 4:10-12, why did Jesus speak in parables?',
-      options: [
-        'The use of simpler terms helped illustrate complex messages to the people.',
-        'So that people outside his inner circle would not understand.',
-        'So that people would engage and ask him questions for clarification.',
-        'He was not able to speak plainly to the public.'
-      ],
-      correct: 'So that people outside his inner circle would not understand.',
-      explanation: "When [Jesus] was alone, those who were around him along with the twelve asked him about the parables. And he said to them, “To you has been given the secret of the kingdom of God, but for those outside everything comes in parables, in order that ‘they may indeed look but not perceive, and may indeed hear but not understand; so that they may not turn again and be forgiven.’”\nMark 4:10-12 (NRSVUE)",
-    };
+    const questions = [
+        {
+          text: 'According to Mark 4:10-12, why did Jesus speak in parables?',
+          options: [
+            'The use of simpler terms helped illustrate complex messages to the people.',
+            'So that people outside his inner circle would not understand.',
+            'So that people would engage and ask him questions for clarification.',
+            'He was not able to speak plainly to the public.'
+          ],
+          correct: 'So that people outside his inner circle would not understand.',
+          explanation: "When [Jesus] was alone, those who were around him along with the twelve asked him about the parables. And he said to them, “To you has been given the secret of the kingdom of God, but for those outside everything comes in parables, in order that ‘they may indeed look but not perceive, and may indeed hear but not understand; so that they may not turn again and be forgiven.’”\nMark 4:10-12 (NRSVUE)",
+          image: markPic,
+        },
+        {
+          text: 'According to the Gospel of John, what is the main message that Jesus preached?',
+          options: [
+            'To prepare the way of the Messiah.',
+            'That the kingdom of God is coming and all should repent.',
+            'He preached about himself and the need to believe in him.',
+            'That only those with the secret knowledge about how the universe works can be saved.'
+          ],
+          correct: 'He preached about himself and the need to believe in him.',
+          explanation: "Instead of speaking in parables and short sayings about the kingdom of God, Jesus speaks in long, difficult monologues about himself, his relation to God, and the need to believe in him (Attridge 1814).",
+          image: johnPic,
+        },
+        {
+          text: 'In Luke 18:9-14, Jesus tells a parable about two men who went up to the temple to pray. Based on the parable alone, how can somebody be justified?',
+          options: [
+            'To believe in Jesus.',
+            'To keep the commandments.',
+            'To have faith in the atonement of the sacrifice of Christ.',
+            'To ask God for repentance.'
+          ],
+          correct: 'To ask God for repentance.',
+          explanation: "He also told this parable to some who trusted in themselves that they were righteous and regarded others with contempt: “Two men went up to the temple to pray, one a Pharisee and the other a tax collector. The Pharisee, standing by himself, was praying thus, ‘God, I thank you that I am not like other people: thieves, rogues, adulterers, or even like this tax collector. I fast twice a week; I give a tenth of all my income.’ But the tax collector, standing far off, would not even lift up his eyes to heaven but was beating his breast and saying, ‘God, be merciful to me, a sinner!’ I tell you, this man went down to his home justified rather than the other, for all who exalt themselves will be humbled, but all who humble themselves will be exalted.”\nLuke 18:9-14 (NRSVUE)",
+          image: lukePic,
+        }
+    ];
+  
+    const currentQuestion = questions[currentQuestionIndex];
   
     const handleSubmit = () => {
       if (selectedAnswer) {
-        if (selectedAnswer === question.correct) {
+        if (selectedAnswer === currentQuestion.correct) {
           setFeedback('Correct!');
         } else {
           setFeedback('Incorrect.');
@@ -33,16 +65,17 @@ const ProjectPreview = () => {
       setSelectedAnswer('');
       setFeedback('');
       setShowExplanation(false);
+      setCurrentQuestionIndex((prevIndex) => (prevIndex + 1) % questions.length);
     };
   
     return (
       <div className="p-5 flex flex-col items-center">
         <div className="w-full mx-auto h-64 mb-10 flex items-center justify-center">
-            <img src={questionPic} className="h-full w-full object-cover" />
+            <img src={currentQuestion.image} className="h-full w-full object-cover" />
         </div>
-        <h1 className="text-xl font-bold mb-4">{question.text}</h1>
+        <h1 className="text-xl font-bold mb-4 max-w-[600px]">{currentQuestion.text}</h1>
         <div className="flex-col">
-        {question.options.map((option) => (
+        {currentQuestion.options.map((option) => (
           <div key={option} className="text-xl mb-2 text-left">
             <input
               type="radio"
@@ -52,21 +85,23 @@ const ProjectPreview = () => {
               onChange={(e) => setSelectedAnswer(e.target.value)}
               className="mr-2"
             />
-            <label>{option}</label>
+            <label onClick={() => setSelectedAnswer(option)} className="cursor-pointer">{option}</label>
           </div>
         ))}
         </div>
-        <button 
-          onClick={handleSubmit} 
-          className="text-xl block mt-4 bg-blue-500 text-white py-2 px-4 rounded"
-        >
-          Submit
-        </button>
+        {!showExplanation && (
+          <button 
+            onClick={handleSubmit} 
+            className="text-xl block mt-4 bg-blue-500 text-white py-2 px-4 rounded"
+          >
+            Submit
+          </button>
+        )}
         {showExplanation && (
           <div className="mt-4">
             <p className="text-xl font-bold mb-4">{feedback}</p>
             <p className ="text-xl max-w-[600px]">
-              {question.explanation.split('\n').map((line, index) => (
+              {currentQuestion.explanation.split('\n').map((line, index) => (
                 <React.Fragment key={index}>
                   {line}
                   <br />
@@ -83,6 +118,6 @@ const ProjectPreview = () => {
         )}
       </div>
     );
-  };
+};
 
 export default ProjectPreview;
--- src/SecondPage.js
+++ src/SecondPage.js
@@ -1,43 +1,53 @@
 import React, { useState } from 'react';
+import caliPic from './assets/IMG_7812.jpg';
+import authentifiedPic from './assets/IMG_7814.jpg';
+import stylePic from './assets/IMG_7810.jpg';
+import gamePic from './assets/IMG_7807Replace.jpg';
 
 const SecondPage = () => {
-  const [highlightedBoxId, setHighlightedBoxId] = useState(null);
+  const [selectedBox, setSelectedBox] = useState({
+    id: 1,
+    image: caliPic,
+    description: '<strong>Front End Intern</strong><br><br>Contributed to the development of an open-source educational website, providing study materials for civil engineering licensure exams<br><br>Developed a page displaying images from the ASCE7 textbook, enabling visitors to view textbook excerpts online for study purposes'
+  });
+
   const boxes = [
-    { id: 1},
-    { id: 2},
-    { id: 3}
+    { id: 1, image: caliPic, title: "Front End Intern", description: '<strong>Front End Intern</strong><br><br>Contributed to the development of an open-source educational website, providing study materials for civil engineering licensure exams<br><br>Developed a page displaying images from the ASCE7 textbook, enabling visitors to view textbook excerpts online for study purposes' },
+    { id: 2, image: authentifiedPic, title: "Acquisition Specialist", description: '<strong>Acquisition Specialist</strong><br><br>Promoted our services to seniors and recent graduates, as well as up and coming companies<br><br>Services included providing endorsements to other companies, and connecting companies with our verified and qualified members' },
+    { id: 3, image: stylePic, title: "Virtual Wardrobe", description: "<strong>Virtual Wardrobe</strong><br><br>Developed an application that could generate outfits in a team for the Senior Design Project I & II courses<br><br>Designed an algorithm to generate outfits with the uploaded clothing of the user based on weather and style preferences, and to assign outfits to specific calendar dates<br><br>Stylized the home and calendar planner pages using CSS and TypeScript<br><br>Click here to view test footage of the app: <a href='https://youtu.be/iSm62BElw0g?si=pMa9LJCAGyBJICMg'>https://youtu.be/iSm62BElw0g?si=pMa9LJCAGyBJICMg</a>" },
+    { id: 4, image: gamePic, title: "2D Video Game with Dynamic Pursuit", description: "<strong>2D Video game with Dynamic Pursuit</strong><br><br>Developed a 2D video game where players dodge and shoot at enemy sprites for the Computer Game Design course<br><br>Enabled users to adjust difficulty settings<br><br>Utilized pixel art to design sprites and backgrounds, achieving a retro style<br><br>Click here to view gameplay: <a href='https://www.youtube.com/watch?v=NgYA5FFwkfo'>https://www.youtube.com/watch?v=NgYA5FFwkfo</a>" }
   ];
-  const toggleHighlight = (id) => {
-    if (highlightedBoxId === id) {
-      setHighlightedBoxId(null);
-    } else {
-      setHighlightedBoxId(id);
-    }
+
+  const handleBoxClick = (box) => {
+    setSelectedBox(box);
   };
 
   return (
-  <div className="p-4">
-    <div className="flex items-center justify-center grid grid-cols-2 gap-4 mb-8">
-      <div className="bg-gray-300 w-full mx-auto h-80 mb-4 flex items-center justify-center text-xl">
-        (IMAGE)
+    <div className="p-4">
+      <div className="flex items-center justify-center grid grid-cols-2 gap-4 mb-8">
+        <div className="bg-gray-300 w-full mx-auto mx-h-500 mb-4 flex items-center justify-center text-xl">
+          <img src={selectedBox.image} alt="Selected" className="h-full w-full object-cover" />
+        </div>
+        <p className="text-xl mb-4 text-left ml-6 mr-6" dangerouslySetInnerHTML={{ __html: selectedBox.description }}></p>
       </div>
-      <p className="text-xl mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
 
-    <div className="grid grid-cols-3 gap-4 mb-8">
+      <div className="grid grid-cols-4 gap-4 mb-8">
         {boxes.map((box) => (
           <div
             key={box.id}
-            className={`bg-gray-300 p-4 h-32 flex items-center justify-center text-xl ${highlightedBoxId === box.id ? 'bg-red-400' : ''}`}
-            onClick={() => toggleHighlight(box.id)}
+            className={`p-4 h-24 flex items-center justify-center text-xl cursor-pointer ${selectedBox.id === box.id ? 'bg-green-400' : 'bg-gray-300'}`}
+            onClick={() => handleBoxClick(box)}
           >
-            (IMAGE)
+            <p className="text-center">{`${box.title}`}</p>
           </div>
         ))}
       </div>
-    <p className="text-xl mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  </div>
-);
+
+      <p className="text-xl mb-4">
+        This page showcases my experience and projects. Click on the images above to learn more about each one.
+      </p>
+    </div>
+  );
 }
 
 export default SecondPage;
